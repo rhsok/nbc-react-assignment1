3343628739e7f65c2e9e75605e44c1579d47b773
sinra <117060851+rhsok@users.noreply.github.com>
2022-12-07 17:48:30 +0900

    css수정

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -7,8 +7,15 @@ import React from "react";
 function TodoItem(props) {
   return (
     <div className="todo-item">
-      <p>{props.item.title}</p>
-      <p>{props.item.content}</p>
+      <div className="todoinner">
+        <div className="todotitle">{props.item.title}</div>
+        <div></div>
+        <div>{props.item.content}</div>
+      </div>
+      <div className="todobtn">
+        <button>삭제하기</button>
+        <button>완료</button>
+      </div>
     </div>
   );
 }
